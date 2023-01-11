59f0e9395c4f970b7d4d3fd98911f76f2f7f0ba2
Gwendal <gwendal.carrique@gmail.com>
2023-01-11 09:58:41 +0100

    Création de 5 consoles dans le main

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,9 +1,15 @@
 package test;
 
+import model.Console;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Console c1 = new Console("Nintendo 64");
+		Console c2 = new Console("Gamecube");
+		Console c3 = new Console("Wii");
+		Console c4 = new Console("Wii U");
+		Console c5 = new Console("Switch");
 
 	}
 
